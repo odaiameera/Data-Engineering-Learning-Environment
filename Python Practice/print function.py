34fffecfb3e1f11979ab459a485be7b4e8517a95
odaiameera <odai@odaiameera.com>
2026-01-08 08:19:11 +0000

    Uploading .gitignore

---
+++ Python Practice/print function.py
@@ -0,0 +1,15 @@
+print('This is a strong{}'.format(' INDEED!'))
+
+print ('The {2} {1} {0}'.format('quick', 'brown', 'fox'))
+print ('The {q} {b} {f}'.format(q='quick', b='brown', f='fox'))
+result = 100 / 777
+result
+print('The result was {r:1.2f}'.format(r=result))
+
+
+name = 'Jose'
+print(f'Hello, his name is {name}')
+print(f"Hello, his name is {name}")
+
+
+print('Python {}'.format('Rules!'))
